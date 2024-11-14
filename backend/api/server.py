f310749bc0b112c148bbb8b58579148376f1b937
Emmanuel <emmanuelumanagarcia@gmail.com>
2024-11-14 01:32:48 +0100

    deploy

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from .controllers.support_request_controller import router as support_request_router
 
 origins = [
+    "http://127.0.0.1:8000",
     "http://localhost:5173",
+    'https://notifier.fly.dev'
 ]
 
 api = FastAPI()
@@ -17,3 +21,10 @@ api.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+api.mount("/", StaticFiles(directory="public", html=True), name="public")
+
+# Serving static files
+@api.get("/{full_path:path}")
+async def serve_react_app():
+    return FileResponse('public/index.html')
